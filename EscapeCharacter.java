cd4b1f1ea97812c90432f3f80db69f8b9c0d8dee
jahswill604 <jahswill604@gmail.com>
2026-05-07 16:14:48 +0100

    PrimitiveDataType

---
+++ EscapeCharacter.java
@@ -0,0 +1,12 @@
+public class EscapeCharacter{
+	public static void main(String[] args){
+		System.out.print("When you need to use the escape characters \n");
+		System.out.print("Use it when you want to manipulate your cursor\n");
+	    System.out.print("Use \\n when you want a newline\n");
+		System.out.print("Who's  is the father of Java?. the father of java is \"James Gosling\"\n");
+		System.out.print("Use this when you want a tab or indentation");
+		System.out.print("We are using carriage return here");
+				
+	
+	}
+}
